bf8f04ef874f1cd7a1af59702c64f8241e9aee75
Namibo06 <namibo654@gmail.com>
2024-12-06 12:45:52 -0300

    feat: importing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterModule } from './pages/register/register.module';
+import { LoginModule } from './pages/login/login.module';
+import { HomeModule } from './pages/home/home.module';
+import { PerfilModule } from './pages/perfil/perfil.module';
+import { LifeFinancialModule } from './pages/life-financial/life-financial.module';
+import { FinancialModule } from './pages/financial/financial.module';
+import { GoalsModule } from './pages/goals/goals.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RegisterModule,
+    LoginModule,
+    HomeModule,
+    PerfilModule,
+    LifeFinancialModule,
+    FinancialModule,
+    GoalsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
